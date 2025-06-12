5814c2b7b7c0b2d1e70568defd3e79505bed7def
farhan-Ali007 <farhanali39765500@gmail.com>
2025-06-12 12:09:16 +0500

    added netlify url for testing

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(xss())
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://my.etimadmart.com'],
+    origin: ['http://localhost:5173', 'https://my.etimadmart.com','https://etimadmart.netlify.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
